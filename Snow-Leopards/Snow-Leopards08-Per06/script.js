7fbaaf52322682878994e58e2053b1969df930da
David Coopersmith <dcoopersmith@mvusd.net>
2024-12-12 09:29:17 -0800

    started javascript dev on quiz.html

---
+++ Snow-Leopards/Snow-Leopards08-Per06/script.js
@@ -0,0 +1,11 @@
+let username = "1234";
+let input = prompt("Enter the secret username: ");
+
+if ( input === username  )
+{
+    alert("Yes!! You got it!");
+}
+else
+{
+    alert("Access denied!");
+}

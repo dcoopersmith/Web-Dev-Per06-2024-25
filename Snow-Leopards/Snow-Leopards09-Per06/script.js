896117ac84fc40517632632c58125cb4d28d563a
David Coopersmith <dcoopersmith@mvusd.net>
2024-12-16 13:25:32 -0800

    added version 09

---
+++ Snow-Leopards/Snow-Leopards09-Per06/script.js
@@ -0,0 +1,12 @@
+let username = "1234";
+let username_hashed = "81dc9bdb52d04dc20036dbd8313ed055";
+let input = prompt("Enter the secret username: ");
+
+if ( input === username  )
+{
+    alert("Yes!! You got it!");
+}
+else
+{
+    alert("Access denied!");
+}

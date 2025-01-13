bdb50982c2b8a094d700c2b9c244ad04e9f65743
David Coopersmith <dcoopersmith@mvusd.net>
2025-01-13 13:48:36 -0800

    started the options for the questions

--- Snow-Leopards/Snow-Leopards10-Per06/script.js
+++ Snow-Leopards/Snow-Leopards10-Per06/script.js
@@ -7,16 +7,35 @@ const Questions = [
             {text: "in the city.", isCorrect: false},
            ]
     },
+    {
+        q: "Fill in the blank: Snow leopards have _____.",
+        a: [{text: "red, with whitish fur.", isCorrect: false},
+            {text: "whitish-gray fur.", isCorrect: true},
+            {text: "black stripes, with orange fur.", isCorrect: false},
+            {text: "white dots with black fur.", isCorrect: false},
+           ]
+    },
+    
     // add more questions here....
 ]; 
 
-let currQuestion = 0;
+let currQuestion = 1;
 let score = 0;
 
 function loadQues() 
 {
     const question = document.getElementById("ques");
-    question.textContent = "Palm Middle School";
+    const opt = document.getElementById("opt");
+
+    question.textContent = Questions[currQuestion].q;
+    opt.innerHTML = "";
+
+    for (          )
+    {
+        
+    }
+
+
 }
 
 loadQues();
